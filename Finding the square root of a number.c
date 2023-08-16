ce29d412d3dc9b2c70c7d1c2139a363f2c999371
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-16 18:59:33 +0530

    Add files via upload

---
+++ Finding the square root of a number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+int n, x;
+printf("Enter the number:\n");
+scanf("%d",&n);
+x=sqrt(n);
+printf("The square root of %d is %d",n,x);
+
+
+return 0;
+}
