1e995fc8b924954bd97500edb7946fc4ac2a5c7e
Swaroop5222 <141646466+Swaroop5222@users.noreply.github.com>
2023-08-16 19:02:18 +0530

    Add files via upload

---
+++ rounding off a number to the next lower integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+float a;
+printf("Enter the value of a\n");
+scanf("%f",&a);
+printf("nearest value of %f is %.f",a,floor(a));
+getch();
+return 0;
+}
+
+
+
